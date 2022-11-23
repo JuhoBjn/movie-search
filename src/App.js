f517886b39a209642b2bb06e1af694e9ca83a896
Juho Björkman <juho.bjn@gmail.com>
2022-11-23 19:12:14 +0200

    Added files for search and watchlist pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import Navbar from "./components/Navbar";
+import MovieSearch from "./pages/MovieSearch";
+import Watchlist from "./pages/Watchlist";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 const App = () => {
@@ -6,6 +10,14 @@ const App = () => {
     <div className="background">
       <Navbar />
       <h1>Hello and welcome to Movie Search!</h1>
+      <Switch>
+        <Route path="/" exact>
+          <MovieSearch />
+        </Route>
+        <Route path="/watchlist">
+          <Watchlist />
+        </Route>
+      </Switch>
     </div>
   );
 };
