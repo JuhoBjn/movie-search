95f82dddf9d61a6bc0ba89b59f996920d4658edb
Juho Björkman <juho.bjn@gmail.com>
2022-11-25 20:03:43 +0200

    Changed JSX for styling

--- src/components/MovieSmall.js
+++ src/components/MovieSmall.js
@@ -1,11 +1,19 @@
+import "./MovieSmall.css";
+
 const MovieSmall = (props) => {
   return (
-    <li>
-      <h2>Title: {props.title}</h2>
-      <h3>Year: {props.year}</h3>
-      <p>Type: {props.type}</p>
-      <img src={props.poster} alt="Poster" />
-    </li>
+    <div className="MovieSmall">
+      {props.poster !== "N/A" && (
+        <div className="moviePoster">
+          <img src={props.poster} alt="Poster" />
+        </div>
+      )}
+      <div className="movieContent">
+        <h2>{props.title}</h2>
+        <h3>Released: {props.year}</h3>
+        <p>Type: {props.type}</p>
+      </div>
+    </div>
   );
 };
 
